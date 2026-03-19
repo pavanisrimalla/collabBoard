32ca3fbc6a7bb6d0ba3f1901856d6597cbf5ffd7
Pavani <pavanisrimalla@gmail.com>
2026-03-19 22:24:17 +0530

    Update MongoDB connection string

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/whiteboardDB");
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://collabBoard:collabBoard123@ac-m1finsp-shard-00-00.neqnjpl.mongodb.net:27017,ac-m1finsp-shard-00-01.neqnjpl.mongodb.net:27017,ac-m1finsp-shard-00-02.neqnjpl.mongodb.net:27017/whiteboardDB?ssl=true&replicaSet=atlas-f8kuxr-shard-0&authSource=admin&appName=Cluster0");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("Database connection error:", error);
